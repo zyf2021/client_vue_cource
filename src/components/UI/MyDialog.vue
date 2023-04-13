f2b0191ac9d9675d49f4be3907a4102cae6de48a
zyf2021 <79312743+zyf2021@users.noreply.github.com>
2023-04-13 22:22:17 +0300

    Лайки, проброс от ребнка к родителю через несколько компонентов

---
+++ src/components/UI/MyDialog.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="dialog" v-if="show">
+        <div class="dialog__content">
+            
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'my-dialog',
+    props: {
+        show: {
+            type:Boolean,
+            default: false
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
