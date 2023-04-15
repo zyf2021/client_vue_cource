662c0eeea7adcadba6a3dbb28c7c301f835b11f3
zyf2021 <79312743+zyf2021@users.noreply.github.com>
2023-04-15 08:49:05 +0300

    пагинация

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,14 +1,17 @@
 <template>
-    <div v-show="posts.length > 0">
-        <post-item 
-            v-for="post in posts" 
-            :post="post"
-            :key="post.id"
-            @remove="removePost(post)"
-            @add="addLike"
-        />
-    </div>
-    <h3 v-show="posts.length == 0">Список пуст</h3>
+        <div v-show="posts.length > 0">
+            <transition-group name="post-list">
+                <post-item 
+                    v-for="post in posts" 
+                    :post="post"
+                    :key="post.id"
+                    @remove="removePost(post)"
+                    @add="addLike"
+                />
+            </transition-group>
+        </div>
+        <h3 v-show="posts.length == 0">Список пуст</h3>
+
 </template>
 
 <script>
@@ -42,5 +45,20 @@ export default {
 </script>
 
 <style>
-
+    .post-list-item {
+    display: inline-block;
+    margin-right: 10px;
+    }
+    .post-list-enter-active,
+    .post-list-leave-active {
+    transition: all 0.4s ease;
+    }
+    .post-list-enter-from,
+    .post-list-leave-to {
+    opacity: 0;
+    transform: translateX(100px);
+    }
+    .post-list-move {
+        transition: transform 0.4s ease;
+    }
 </style>
---
+++ src/components/UI/MySelect.vue
@@ -0,0 +1,44 @@
+<template>
+  <select class="select" :value="modelValue" @change="changeOption">
+    <option disabled value = "">Выберите из списка</option>
+    <option
+        v-for="option in options"
+        :key="option.value"
+        :value="option.value"
+    >
+        {{ option.name }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+    name: "my-select",
+    props: {
+        modelValue: {
+            type: String
+        },
+        options: {
+            type: Array,
+            default: () => []
+        }
+    },
+    methods: {
+        changeOption(event) {
+            this.$emit('update:modelValue', event.target.value);
+        }
+    }
+}
+</script>
+
+<style scoped>
+    .select {
+        margin-top: 15px;
+        margin-right: 5px;
+        align-self: flex-end;
+        padding: 9px;
+        background: None;
+        color: teal;
+        border: 2px solid teal;
+    }
+</style>
